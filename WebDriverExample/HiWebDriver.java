f9d321979c982efaf2e466a7f9b34f0c29644981
Yang Wu <wenyangwu168@gmail.com>
2024-08-23 22:54:37 -0500

    one more change

--- WebDriverExample/HiWebDriver.java
+++ WebDriverExample/HiWebDriver.java
@@ -15,6 +15,9 @@ public class HiWebDriver {
 
 	public static void main(String[] args) {
         System.out.println("hello class");
+        System.out.println("hello class");
+        System.out.println("hello class");
+        System.out.println("hello class");
         
         try {
             WebDriver driver =  new ChromeDriver();
